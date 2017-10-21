645539b653e437a1a0b789d5136d966a95c71305
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-10-21 11:42:02 +0530

    To illustrate the concept of Anonymous Inner class in java.

---
+++ AnonymousInner2.java
@@ -0,0 +1,22 @@
+public class AnonymousInner2
+{
+  public static void main(String[] args)
+  {
+    Runnable r=new Runnable()
+    {  
+      public void run()
+      {
+        for(int i=0;i<10;i++)
+           System.out.println("child thread ");
+        
+      }
+    };
+  
+    Thread t=new Thread(r);
+    t.start();
+    
+	for(int i=0;i<10;i++)
+       System.out.println("Main thread ");
+    
+  }
+}
