aa3ad658516bc7bc88a1d3907a02a41acd918960
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-10-22 11:23:04 +0530

    To illustrate the concept of Static Nested class in java.

---
+++ StaticNested2.java
@@ -0,0 +1,15 @@
+class StaticNested2
+{
+  static class Nested
+  {
+    public static void main(String[] args)
+    {
+      System.out.println("static nested class main method ");
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    System.out.println(" outer class main method ");
+  }
+} 
